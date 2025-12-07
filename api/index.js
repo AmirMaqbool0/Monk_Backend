ddd1dcb4103a4f61702f205bd8c511e301741e06
AmirMaqbool0 <amirmaqbool055@gmail.com>
2025-12-07 18:38:12 +0500

    fix-deployment-issue3.0

--- api/index.js
+++ api/index.js
@@ -1,28 +1,15 @@
-// api/index.js — Express + Vercel (Serverless Function)
 import serverless from "serverless-http";
 import app from "../src/app.js";
 import connectDB from "../src/config/db.js";
-import dotenv from "dotenv";
 
-dotenv.config();
+let connected = false;
 
-let isConnected = false;
-
-// Vercel Serverless handler
 export default async function handler(req, res) {
-  try {
-    if (!isConnected) {
-      await connectDB();
-      isConnected = true;
-    }
-  } catch (err) {
-    console.error("DB Connection Error:", err);
-    return res.status(500).json({ error: "Database connection error" });
+  if (!connected) {
+    await connectDB();
+    connected = true;
   }
 
-  // Convert Express to a serverless handler
   const expressHandler = serverless(app);
-
-  // Execute it
   return expressHandler(req, res);
 }
